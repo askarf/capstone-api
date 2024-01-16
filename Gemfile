69850087ca5fd734fd917e71f1d27cc0214df8da
askarf <ayalakarf@gmail.com>
2024-01-16 16:03:11 -0500

    adds carrierwave gem

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem "bcrypt", "~> 3.1.7"
 gem "rack-cors"
 gem "jwt"
 gem "seed_dump"
+gem "carrierwave"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,9 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :size, presence: true
-  validates :image, presence: true
   validates :condition, presence: true
-  validates :retail_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :selling_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :retail_price, presence: true
+  validates :selling_price, presence: true
 
   belongs_to :user
   has_many :images
